68c0a0bd7238e5cfcb953bc09179a5884c543bb7
andrew <andrewh23@protonmail.com>
2022-07-18 10:16:16 -0700

    create html, js and css file

---
+++ my-script.js
@@ -0,0 +1,6 @@
+// Parent Element 
+const space = document.createElement("div");
+space.setAttribute("id", "calculator");
+space.setAttribute("class", "container");
+const getCalc = document.getElementsById("calculator");
+const getCont = document.querySelector(".container")
