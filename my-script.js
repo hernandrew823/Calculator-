45a5b4af926d7b2c865d0b6788b08f400282c203
andrew <andrewh23@protonmail.com>
2022-07-18 10:54:00 -0700

    add child elment for display and buttons

--- my-script.js
+++ my-script.js
@@ -3,4 +3,26 @@ const space = document.createElement("div");
 space.setAttribute("id", "calculator");
 space.setAttribute("class", "container");
 const getCalc = document.getElementsById("calculator");
-const getCont = document.querySelector(".container")
+const getCont = document.querySelector(".container");
+
+//Create Display Children 
+const display = document.createElement("div"); 
+display.setAttribute("id", "showInput");
+display.setAttribute("class", "inputDisplay")
+
+
+//Draw Button Children
+function makeRows(x, y) {
+space.style.setProperty('--grid-rows', x);
+space.style.setProperty('--grid-columns', y);
+
+for ( let c = 0; c < ( x * y); c++) {
+    const filling = document.createElement("div"); 
+calculator.appendChild(filling).className = "button";
+filling.setAttribute("id", "calcKey");
+
+
+}
+
+
+}
