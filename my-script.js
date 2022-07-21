96c5a68222a0165ee43b87b5e6a08d953bbaa2c9
andrew <andrewh23@protonmail.com>
2022-07-21 14:31:45 -0700

    have display show inputs for a, b, o and answr arrays

--- my-script.js
+++ my-script.js
@@ -16,61 +16,61 @@ function touchA() {
     document.getElementById("calcKey1").addEventListener( "click", function(){
     if ( o.length < 1) {
         a.push(1);
-        document.getElementById('screen').innerHTML = a;
+        document.getElementById('screen').innerHTML = a.join('');
 }})
 
 document.getElementById("calcKey2").addEventListener( "click", function(){
     if ( o.length < 1) {
     a.push(2);
-    document.getElementById('screen').innerHTML = a;
+    document.getElementById('screen').innerHTML = a.join('');
 }})
 
 document.getElementById("calcKey3").addEventListener( "click", function(){
     if ( o.length < 1) {
     a.push(3);
-    document.getElementById('screen').innerHTML = a;
+    document.getElementById('screen').innerHTML = a.join('');
 }})
 
 document.getElementById("calcKey4").addEventListener( "click", function(){
     if ( o.length < 1) {
     a.push(4);
-    document.getElementById('screen').innerHTML = a;
+    document.getElementById('screen').innerHTML = a.join('');
 }})
 
 document.getElementById("calcKey5").addEventListener( "click", function(){
     if ( o.length < 1) {
     a.push(5);
-    document.getElementById('screen').innerHTML = a;
+    document.getElementById('screen').innerHTML = a.join('');
 }})
 
 document.getElementById("calcKey6").addEventListener( "click", function(){
     if ( o.length < 1) {
     a.push(6);
-    document.getElementById('screen').innerHTML = a;
+    document.getElementById('screen').innerHTML = a.join('');
 }})
 
 document.getElementById("calcKey7").addEventListener( "click", function(){
     if ( o.length < 1) {
     a.push(7);
-    document.getElementById('screen').innerHTML = a;
+    document.getElementById('screen').innerHTML = a.join('');
 }})
 
 document.getElementById("calcKey8").addEventListener( "click", function(){
     if ( o.length < 1) {
     a.push(8);
-    document.getElementById('screen').innerHTML = a;
+    document.getElementById('screen').innerHTML = a.join('');
 }})
 
 document.getElementById("calcKey9").addEventListener( "click", function(){
     if ( o.length < 1) {
     a.push(9);
-    document.getElementById('screen').innerHTML = a;
+    document.getElementById('screen').innerHTML = a.join('');
 }})
 
 document.getElementById("calcKey0").addEventListener( "click", function(){
     if ( o.length < 1) {
     a.push(0);
-    document.getElementById('screen').innerHTML = a;
+    document.getElementById('screen').innerHTML = a.join('');
 }})
 
 }
@@ -90,20 +90,24 @@ document.getElementById("calcKeyPlus").addEventListener( "click", function(){
     if (a.length >= 1) {
     o.push('+');
     console.log(o);
+    document.getElementById('screen').innerHTML = o.join('')
 }})
 document.getElementById("calcKeyMinus").addEventListener( "click", function(){
     if (a.length >= 1)  {
     o.push('-');
   console.log(o);
+  document.getElementById('screen').innerHTML = o.join('')
 }})
 document.getElementById("calcKeyTimes").addEventListener( "click", function(){
     if (a.length >= 1) {
     o.push('*');
 console.log(o);
+document.getElementById('screen').innerHTML = o.join('')
 }})
 document.getElementById("calcKeyDivide").addEventListener( "click", function(){
     if (a.length >= 1)  { o.push('/');
 console.log(o);
+document.getElementById('screen').innerHTML = o.join('')
 }})
 }
 
@@ -114,24 +118,31 @@ function touchB() {
         if (o.length >= 1){
         b.push(1);
         console.log(b);
+        document.getElementById('screen').innerHTML = b.join('')
     }})
     
     document.getElementById("calcKey2").addEventListener( "click", function(){
         if (o.length >= 1) {
         b.push(2);
         console.log(b);
+        document.getElementById('screen').innerHTML = b.join('')
+
     }})
     
     document.getElementById("calcKey3").addEventListener( "click", function(){
         if (o.length >= 1) {
         b.push(3);
         console.log(b);
+        document.getElementById('screen').innerHTML = b.join('')
+
     }})
     
     document.getElementById("calcKey4").addEventListener( "click", function(){
         if (o.length >= 1) {
         b.push(4);
         console.log(b);
+        document.getElementById('screen').innerHTML = b.join('')
+
     }})
     document.getElementById("calcKey5").addEventListener( "click", function(){
         if (o.length >= 1) {
@@ -143,29 +154,39 @@ function touchB() {
         if (o.length >= 1) {
         b.push(6);
         console.log(b);
+        document.getElementById('screen').innerHTML = b.join('')
+
     }})
     document.getElementById("calcKey7").addEventListener( "click", function(){
         if (o.length >= 1) {
         b.push(7);
         console.log(b);
+        document.getElementById('screen').innerHTML = b.join('')
+
     }})
 
     document.getElementById("calcKey8").addEventListener( "click", function(){
         if (o.length >= 1) {
         b.push(8);
         console.log(b);
+        document.getElementById('screen').innerHTML = b.join('')
+
     }})
     
     document.getElementById("calcKey9").addEventListener( "click", function(){
         if (o.length >= 1) {
         b.push(9);
         console.log(b);
+        document.getElementById('screen').innerHTML = b.join('')
+
     }})
 
     document.getElementById("calcKey0").addEventListener( "click", function(){
         if (o.length >= 1) {
         b.push(0);
         console.log(b);
+        document.getElementById('screen').innerHTML = b.join('')
+
     }})
     } 
 
@@ -174,18 +195,26 @@ function touchE() {
    e.push("=");
    if ( b.length > 0 && o == "+") { answr += ((parseInt(a.join())) + (parseInt(b.join()))); 
     console.log(answr); 
+    document.getElementById('screen').innerHTML = answr;
+
 }
    if ( b.length > 0 && o == "-") { answr += ((parseInt(a.join())) - (parseInt(b.join())))
-    console.log(answr)
+    console.log(answr);
+    document.getElementById('screen').innerHTML = answr;
+
 }
 
 
    if ( b.length > 0 && o == "*") {answr += ((parseInt(a.join())) * (parseInt(b.join())))
-    console.log(answr)
+    console.log(answr);
+    document.getElementById('screen').innerHTML = answr;
+
 }
 
    if ( b.length > 0 && o == "/") { answr += ((parseInt(a.join())) / (parseInt(b.join())))
-   console.log(answr)
+   console.log(answr);
+   document.getElementById('screen').innerHTML = answr;
+
 }})
 }
 
