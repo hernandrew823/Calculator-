950f7cc95f4cb4d153ce21ebd463733ee7ce90bb
andrew <andrewh23@protonmail.com>
2022-08-12 10:24:57 -0700

    modify operaor function to better chain by modification of expression variables and alter showme function to represent values of joined b2 elements on calculator screen

--- my-script.js
+++ my-script.js
@@ -13,20 +13,24 @@ function showMe() {
     
     document.getElementById('screen').innerHTML = '0';
 
-    if (o1.length < 1) {
+    if (o1.length < 1 && a2.length < 1) {
         document.getElementById('screen').innerHTML = a1.join('');
         
     }
     else if (a1.length >= 1 && b1.length < 1 && o1.length <= 1) {
-        document.getElementById('screen').innerHTML = o1[0];
+        document.getElementById('screen').innerHTML = o1;
     }
     else if (a1.length >= 1 && b1.length < 1 && o1.length > 1) { 
-        document.getElementById('screen').innerHTML =  o1[1];
+        document.getElementById('screen').innerHTML =  o1;
 
 
     }
     else if ( o1.length >= 1 ) {
         document.getElementById('screen').innerHTML = b1.join('');
+    }
+    
+else if ( a2.length >= 1 && o2.length >=1 ) {
+        document.getElementById('screen').innerHTML = b2.join(''); 
     }
     
 }
@@ -39,7 +43,7 @@ function showMe() {
 //Event listner A
 function touchA() { 
     document.getElementById("calcKey1").addEventListener( "click", function(){
-    if ( o1.length < 1) {
+    if ( exOne.length < 1 && o1.length <1) {
         a1.push(1);
         console.log(a1);
         showMe();
@@ -48,7 +52,7 @@ function touchA() {
 })
 
 document.getElementById("calcKey2").addEventListener( "click", function(){
-    if ( o1.length < 1) {
+    if ( exOne.length < 1 && o1.length <1) {
     a1.push(2);
     console.log(a1);
     showMe();
@@ -57,35 +61,35 @@ document.getElementById("calcKey2").addEventListener( "click", function(){
 }})
 
 document.getElementById("calcKey3").addEventListener( "click", function(){
-    if ( o1.length < 1) {
+    if ( exOne.length < 1 && o1.length <1) {
     a1.push(3);
     console.log(a1);
     showMe();
 }})
 
 document.getElementById("calcKey4").addEventListener( "click", function(){
-    if ( o1.length < 1) {
+    if ( exOne.length < 1 && o1.length <1) {
     a1.push(4);
     console.log(a1);
     showMe();
 }})
 
 document.getElementById("calcKey5").addEventListener( "click", function(){
-    if ( o1.length < 1) {
+    if ( exOne.length < 1 && o1.length <1) {
     a1.push(5);
     console.log(a1);
     showMe();
 }})
 
 document.getElementById("calcKey6").addEventListener( "click", function(){
-    if ( o1.length < 1) {
+    if ( exOne.length < 1 && o1.length <1) {
     a1.push(6);
     console.log(a1);
     showMe();
 }})
 
 document.getElementById("calcKey7").addEventListener( "click", function(){
-    if ( o1.length < 1) {
+    if ( exOne.length < 1 && o1.length <1) {
     a1.push(7);
     console.log(a1);
     showMe();
@@ -94,14 +98,14 @@ document.getElementById("calcKey7").addEventListener( "click", function(){
 }})
 
 document.getElementById("calcKey8").addEventListener( "click", function(){
-    if ( o1.length < 1) {
+    if ( exOne.length < 1 && o1.length <1) {
     a1.push(8);
     console.log(a1);
     showMe();
 }})
 
 document.getElementById("calcKey9").addEventListener( "click", function(){
-    if ( o1.length < 1) {
+    if ( exOne.length < 1 && o1.length <1) {
     a1.push(9);
     console.log(a1);
     showMe();
@@ -110,7 +114,7 @@ document.getElementById("calcKey9").addEventListener( "click", function(){
 }})
 
 document.getElementById("calcKey0").addEventListener( "click", function(){
-    if ( o1.length < 1) {
+    if ( exOne.length < 1 && o1.length <1) {
     a1.push(0);
     console.log(a1);
     showMe();
@@ -141,19 +145,28 @@ document.getElementById("calcKeyPlus").addEventListener( "click", function(){
 }
 else if (b1.length >= 1 ) {
     o2.push('+');
-        showMe();
     console.log(exOne);
     console.log(a2);
     console.log(o2);
+    showMe();
+
 
 
 
 }
 else if (o2.length >= 1) {
     o1.push('+');
-    showMe();
     console.log(o2);
     console.log(o1);
+    showMe();
+
+}
+
+else if (a1.length >= 1) {
+    o1.push('+');
+    console.log(o2);
+    console.log(o1);
+    showMe();
 }
 })
 document.getElementById("calcKeyMinus").addEventListener( "click", function(){
@@ -165,11 +178,11 @@ document.getElementById("calcKeyMinus").addEventListener( "click", function(){
     }
     else if (b1.length >= 1 ) {
         o2.push('-');
-            showMe();
         console.log(exOne);
         console.log(a2);
         console.log(o2);
-    
+        showMe();
+
     
     
     }
@@ -190,10 +203,11 @@ document.getElementById("calcKeyTimes").addEventListener( "click", function(){
     }
     else if (b1.length >= 1 ) {
         o2.push('*');
-            showMe();
         console.log(exOne);
         console.log(a2);
         console.log(o2);
+        showMe();
+
     
     
     
@@ -215,19 +229,21 @@ document.getElementById("calcKeyDivide").addEventListener( "click", function(){
     }
     else if (b1.length >= 1 ) {
         o2.push('/');
-            showMe();
         console.log(exOne);
         console.log(a2);
         console.log(o2);
+        showMe();
+
     
     
     
     }
     else if (o2.length >= 1) {
         o1.push('/');
-        showMe();
         console.log(o2);
         console.log(o1);
+        showMe();
+
     }
     })
 }
@@ -285,7 +301,11 @@ else if (a2.length >= 1){
 
     } else if (a2.length >= 1){ 
         b2.push(4);
+        console.log(o2);
         console.log(b2); 
+        console.log(o1);
+        console.log(a2);
+        console.log(exTwo);
         showMe();}})
 
     document.getElementById("calcKey5").addEventListener( "click", function(){
@@ -331,7 +351,9 @@ else if (a2.length >= 1){
 
      else if (a2.length >= 1){ 
         b2.push(8);
-        console.log(b2); 
+        console.log(b2);
+        console.log(a1); 
+        console.log(exTwo)
         showMe();}})
     
     document.getElementById("calcKey9").addEventListener( "click", function(){
@@ -363,7 +385,7 @@ function operator() {                                               //does not a
         item.addEventListener('click', event => {
     
    if ( b1.length >= 1 && o1 == "+") { 
-    a1.splice(0, a1.length, (a1.join('')));
+    a1.push(a1.join(''));
    console.log(a1);
    b1.splice(0, b1.length, (b1.join('')));
    console.log(b1);
@@ -372,6 +394,7 @@ function operator() {                                               //does not a
     a1.splice(0, a1.length);
     o1.splice(0, o1.length);
     b1.splice(0, b1.length);
+    exTwo.splice(0, exTwo.length);
     console.log(a1);
     console.log(exOne)
     console.log(b1);
@@ -383,7 +406,7 @@ function operator() {                                               //does not a
 
 }
 
-else if (b2 >= 1 && o2 == "+") {
+else if (b2.length >= 1 && o2 == "+") {
 
     a2.splice(0, a1.length, (a2.join('')));
    console.log(a2);
@@ -391,15 +414,18 @@ else if (b2 >= 1 && o2 == "+") {
    console.log(b2);
     exTwo.splice(0, exTwo.length, ((parseInt(a2,10)) + (parseInt(b2, 10)))); 
     a1.splice(0, a1.length, ...exTwo);
+    exOne.splice(0, exOne.length);
     a2.splice(0, a2.length);
     o2.splice(0, o2.length);
     b2.splice(0, b2.length);
+    console.log(a1);
+    console.log(exTwo);
     console.log(a2);
-    console.log(exTwo)
     console.log(b2);
     console.log(o2);
     console.log(o1);
-    console.log(a1)
+    console.log(exOne);
+    
 
     document.getElementById('screen').innerHTML = exTwo;
 
@@ -412,6 +438,7 @@ else if ( b1.length >= 1 && o1 == "-") {
    console.log(b1);
     exOne.splice(0, exOne.length, ((parseInt(a1,10)) - (parseInt(b1, 10)))); 
     a2.splice(0, a2.length, ...exOne);
+    exTwo.splice(0, exTwo.length);
     a1.splice(0, a1.length);
     o1.splice(0, o1.length);
     b1.splice(0, b1.length);
@@ -426,7 +453,7 @@ else if ( b1.length >= 1 && o1 == "-") {
 
 }
 
-else if (b2 >= 1 && o2 == "-") {
+else if (b2.length >= 1 && o2 == "-") {
 
     a2.splice(0, a1.length, (a2.join('')));
    console.log(a2);
@@ -434,6 +461,7 @@ else if (b2 >= 1 && o2 == "-") {
    console.log(b2);
     exTwo.splice(0, exTwo.length, ((parseInt(a2,10)) - (parseInt(b2, 10)))); 
     a1.splice(0, a1.length, ...exTwo);
+    exOne.splice(0, exOne.length);
     a2.splice(0, a2.length);
     o2.splice(0, o2.length);
     b2.splice(0, b2.length);
@@ -453,6 +481,7 @@ else if ( b1.length >= 1 && o1 == "*") {
    console.log(b1);
     exOne.splice(0, exOne.length, ((parseInt(a1,10)) * (parseInt(b1, 10)))); 
     a2.splice(0, a2.length, ...exOne);
+    exTwo.splice(0, exTwo.length);
     a1.splice(0, a1.length);
     o1.splice(0, o1.length);
     b1.splice(0, b1.length);
@@ -467,7 +496,7 @@ else if ( b1.length >= 1 && o1 == "*") {
 
 }
 
-else if (b2 >= 1 && o2 == "*") {
+else if (b2.length >= 1 && o2 == "*") {
 
     a2.splice(0, a1.length, (a2.join('')));
    console.log(a2);
@@ -475,6 +504,7 @@ else if (b2 >= 1 && o2 == "*") {
    console.log(b2);
     exTwo.splice(0, exTwo.length, ((parseInt(a2,10)) * (parseInt(b2, 10)))); 
     a1.splice(0, a1.length, ...exTwo);
+    exOne.splice(0, exOne.length);
     a2.splice(0, a2.length);
     o2.splice(0, o2.length);
     b2.splice(0, b2.length);
@@ -495,6 +525,7 @@ else if ( b1.length >= 1 && o1 == "/") {
    console.log(b1);
     exOne.splice(0, exOne.length, ((parseInt(a1,10)) / (parseInt(b1, 10)))); 
     a2.splice(0, a2.length, ...exOne);
+    exTwo.splice(0, exTwo.length);
     a1.splice(0, a1.length);
     o1.splice(0, o1.length);
     b1.splice(0, b1.length);
@@ -509,7 +540,7 @@ else if ( b1.length >= 1 && o1 == "/") {
 
 }
 
-else if (b2 >= 1 && o2 == "/") {
+else if (b2.length >= 1 && o2 == "/") {
 
     a2.splice(0, a1.length, (a2.join('')));
    console.log(a2);
@@ -517,6 +548,7 @@ else if (b2 >= 1 && o2 == "/") {
    console.log(b2);
     exTwo.splice(0, exTwo.length, ((parseInt(a2,10)) / (parseInt(b2, 10)))); 
     a1.splice(0, a1.length, ...exTwo);
+    exOne.splice(0, exOne.length);
     a2.splice(0, a2.length);
     o2.splice(0, o2.length);
     b2.splice(0, b2.length);
